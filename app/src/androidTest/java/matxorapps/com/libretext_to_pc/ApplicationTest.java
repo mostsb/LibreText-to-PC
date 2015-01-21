c1c6f5518e7c11adf6dc117aae545e0bd0b00bd9
mostsb <matthewsmith@linux.com>
2015-01-21 14:49:04 -0800

    Established Repository

---
+++ app/src/androidTest/java/matxorapps/com/libretext_to_pc/ApplicationTest.java
@@ -0,0 +1,13 @@
+package matxorapps.com.libretext_to_pc;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
